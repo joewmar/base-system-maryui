7d62f7eb7169c758c0848d0a10f53da1ec0a6be2
joewmar <recelestino90@gmail.com>
2024-03-11 09:07:31 +0800

    Commit

--- resources/views/livewire/requisition/weekly-requisition-home.blade.php
+++ resources/views/livewire/requisition/weekly-requisition-home.blade.php
@@ -6,29 +6,37 @@
     <div class="p-4 text-center font-bold text-3xl">
         <h3>Weekly Requisition</h3>
     </div>
-    {{-- Search Button --}}
-    <div class="flex justify-start my-5 p-3 ">
-        <div><x-input label="Search" inline icon="o-magnifying-glass" wire:model="search" type="search" class="input-sm" /></div>
-    </div>
-    {{-- Table --}}
-    <x-table :headers="$headers" :rows="$weeklyOrders" striped with-pagination>
-        @scope('actions', $weeklyOrder)
-            <div class="flex justify-around w-full space-x-2">
-                <x-button tooltip="View" label="View" class="bg-green-500 btn-sm" @click="$wire.editModal = true" />
+    <x-tabs selected="table-tab">
+        <x-tab name="table-tab" label="Show List" icon="o-list-bullet">
+            <div>Table for Requisition</div>
+            
+        </x-tab>
+        <x-tab name="add-tab" label="Create New" icon="c-archive-box">
+             {{-- Search Button --}}
+            <div class="flex justify-start my-5 p-3 ">
+                <div><x-input label="Search" inline icon="o-magnifying-glass" wire:model="search" type="search" class="input-sm" /></div>
             </div>
-        @endscope
-    </x-table>
-    {{-- Edit Modal --}}
-    <x-modal wire:model="editModal" title="Weekly Orders" separator >
-        <div class="flex flex-col space-y-3">
-        <div><x-input label="Price per Kilograms" wire:model.live='price_per_kg' inline /></div>
-        <div><x-input label="Inventory Cost" wire:model.live='inventory_cost' inline /></div>
-        <div><x-input label="Kilograms per bag" wire:model.live='kilograms_per_bag' inline /></div>
-        <div><x-input label="STD days" wire:model.live='standard_days' inline /></div>
-        </div>
-        <x-slot:actions>
-            <x-button label="Cancel" @click="$wire.editModal = false" />
-            <x-button label="Update" class="bg-green-500 hover:bg-green-600" icon="c-pencil-square"/>
-        </x-slot:actions>
-    </x-modal>
+            {{-- Table --}}
+            <x-table :headers="$headers" :rows="$weeklyOrders" striped with-pagination>
+                @scope('actions', $weeklyOrder)
+                    <div class="flex justify-around w-full space-x-2">
+                        <x-button tooltip="View" label="View" class="bg-green-500 btn-sm" @click="$wire.editModal = true" />
+                    </div>
+                @endscope
+            </x-table>
+            {{-- Edit Modal --}}
+            <x-modal wire:model="editModal" title="Weekly Orders" separator >
+                <div class="flex flex-col space-y-3">
+                <div><x-input label="Price per Kilograms" wire:model.live='price_per_kg' inline /></div>
+                <div><x-input label="Inventory Cost" wire:model.live='inventory_cost' inline /></div>
+                <div><x-input label="Kilograms per bag" wire:model.live='kilograms_per_bag' inline /></div>
+                <div><x-input label="STD days" wire:model.live='standard_days' inline /></div>
+                </div>
+                <x-slot:actions>
+                    <x-button label="Cancel" @click="$wire.editModal = false" />
+                    <x-button label="Update" class="bg-green-500 hover:bg-green-600" icon="c-pencil-square"/>
+                </x-slot:actions>
+            </x-modal>
+        </x-tab>
+    </x-tabs>
 </div>
