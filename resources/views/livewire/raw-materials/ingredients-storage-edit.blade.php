7f1df44c2c1ef9a4d10a6fe6596a5d97eefca3db
JoemariePortacio <joemarieportacio00@gmail.com>
2024-03-11 09:07:40 +0800

    edit ingredients

--- resources/views/livewire/raw-materials/ingredients-storage-edit.blade.php
+++ resources/views/livewire/raw-materials/ingredients-storage-edit.blade.php
@@ -7,26 +7,33 @@
         <h3>Edit Ingredients Storage</h3>
     </div>
     {{-- Calendar-Date --}}
-    <div class="w-48">
+    {{-- Select FeedType --}}
+    <div class=" w-48">
         <x-datetime label="Date" wire:model.live="inv_date" icon="o-calendar" inline />
     </div>
-    {{--  --}}
+
+    {{-- Ingredients Storage --}}
     <div>
-        @foreach ($ingredents as $ingredent)
-            <div class="grid grid-cols-1 md:grid-cols-6 gap-4 pt-5 border p-3 mt-5" >
-                <x-select label="Feed Type" placeholder-value="" placeholder="Select Feed Type" :options="$farms" option-value="id" option-label="farm_name" inline wire:model.live="ingredents.item{{$loop->iteration}}.feed_type_id" />
+ @foreach ($ingredents as $ingredent)
+            <div class="grid grid-cols-1 md:grid-cols-5 gap-4 pt-5 border p-3 mt-5" >
+                <div class="col-span-5 flex justify-between">
+                    <x-select label="Feed Type" placeholder-value="" placeholder="Select Feed Type" :options="$farms" option-value="id" option-label="farm_name" inline />
+                    <div>
+                        {{-- Action Add Item and Delete  --}}
+                       <x-button tooltip="Remove Item" icon="s-trash"  class="mt-1 btn-outline text-red-500 border-red-600 hover:bg-red-500" wire:click="removeItem('{{encrypt('item'.$loop->iteration)}}')" />
+                       @if ($loop->last)
+                           <x-button tooltip="Add Item" icon="o-plus" class="btn-outline text-blue-600 border-blue-600 hover:bg-blue-700  text-sm" wire:click="addItem"  />
+                       @else 
+                           <x-button tooltip="Add Item" icon="o-plus" class="btn-outline text-blue-600 border-blue-600 hover:bg-blue-700  text-sm" wire:click="addItem" disabled />
+                       @endif
+                    </div>
+                </div>
                 <x-input label="Farm" value="None" inline disabled wire:model.live="ingredents.item{{$loop->iteration}}.farm_name"   />
                 <x-input label="Standard" inline wire:model.live="ingredents.item{{$loop->iteration}}.standard" />
                 <x-input label="Batch" inline wire:model.live="ingredents.item{{$loop->iteration}}.batch" />
                 <x-input label="Adjustment" inline wire:model="ingredents.item{{$loop->iteration}}.adjustment" />
-                <div> 
-                    {{-- Action Add Item and Delete --}}
-                    <x-button tooltip="Remove Item" icon="s-trash"  class="mt-1 btn-outline text-red-500 border-red-600 hover:bg-red-500" wire:click="removeItem('{{encrypt('item'.$loop->iteration)}}')" />
-                    @if ($loop->last)
-                        <x-button tooltip="Add Item" icon="o-plus" class="btn-outline text-blue-600 border-blue-600 hover:bg-blue-700  text-sm" wire:click="addItem"  />
-                    @else 
-                        <x-button tooltip="Add Item" icon="o-plus" class="btn-outline text-blue-600 border-blue-600 hover:bg-blue-700  text-sm" wire:click="addItem" disabled />
-                    @endif
+                <div class="flex justify-center pt-4">
+                    <x-checkbox wire:model="item1" />
                 </div>
             </div >
         @endforeach
